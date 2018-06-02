7eebdf0897e43eecb4b2470f6353c34320a63c57
TSConroe <1conroe1@gmail.com>
2018-06-02 13:27:02 +0300

    gitignor deleted

---
+++ src/main/java/tk/remainder/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package tk.remainder.repos;
+
+import tk.remainder.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
